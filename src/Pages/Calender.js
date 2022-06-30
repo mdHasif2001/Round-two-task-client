4352618bd471666b4453501af8c62b1b78487e35
mdHasif2001 <iamhasib727@gmail.com>
2022-07-01 01:44:01 +0600

    calender hasbeen created

--- src/Pages/Calender.js
+++ src/Pages/Calender.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import { format } from 'date-fns';
+import React, { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 
 const Calender = () => {
+    const [date, setDate] = useState(new Date());
     return (
         <div>
-            <h1 className='text-3xl'>This is Calender</h1>
+
+            <div className='flex justify-center align-middle'>
+                <DayPicker
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                />
+            </div>
+            <div>
+                <p className='text-2xl text-center text-indigo-700'>You have selected: {format(date, 'PP')}</p>
+            </div>
         </div>
+
+
+
     );
 };
 
